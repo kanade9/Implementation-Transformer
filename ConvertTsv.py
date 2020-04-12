b46f83008d07c07da798f911c8bcb5b052018657
kzmaro <maronkazu94@gmail.com>
2020-04-12 14:44:21 +0900

    PositionalEncoderの実装完了

---
+++ ConvertTsv.py
@@ -0,0 +1,172 @@
+# for colab
+"""
+! mkdir data
+"""
+
+# Download IMDb dataset
+import glob, os, io, string, urllib.request, tarfile, re, torchtext, random, zipfile
+from torchtext.vocab import Vectors
+
+url = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+save_path = "./data/aclImdb_v1.tar.gz"
+if not os.path.exists(save_path):
+    urllib.request.urlretrieve(url, save_path)
+urllib.request.urlretrieve(url)
+tar = tarfile.open('./data/aclImdb_v1.tar.gz')
+tar.extractall('./data/')
+tar.close()
+
+
+# トレーニングデータのneg,pos,テストデータのneg,posのtsvファイル作成
+
+
+def get_IMDb_DataLoaders_and_TEXT(max_length=256, batch_size=24, debug_log=False):
+    path_list = ['./data/aclImdb/train/pos/', './data/aclImdb/train/neg/', './data/aclImdb/test/pos/',
+                 './data/aclImdb/test/neg/']
+
+    # ここなんか訓練データが半減するので、公式の以下の方法で行う
+
+    for dataset_path in path_list:
+        if 'train' in dataset_path:
+            f = open('./data/IMDb_train.tsv', 'w')
+        else:
+            f = open('./data/IMDb_test.tsv', 'w')
+
+    # ダウンロードしてきたファイルの整形を行う
+    for fname in glob.glob(os.path.join(dataset_path, '*.txt')):
+        with io.open(fname, 'r', encoding="utf-8") as ff:
+            text = ff.readline()
+            text = text.replace('\t', " ")
+            text = text + '\t' + '1' + '\t' + '\n'
+            f.write(text)
+    f.close()
+
+    f = open('./data/IMDb_train.tsv', 'w')
+
+    path = './data/aclImdb/train/pos/'
+
+    for fname in glob.glob(os.path.join(path, '*.txt')):
+        with io.open(fname, 'r', encoding="utf-8") as ff:
+            text = ff.readline()
+    text = text.replace('\t', " ")
+    text = text + '\t' + '1' + '\t' + '\n'
+    f.write(text)
+
+    path = './data/aclImdb/train/neg/'
+    for fname in glob.glob(os.path.join(path, '*.txt')):
+        with io.open(fname, 'r', encoding="utf-8") as ff:
+            text = ff.readline()
+            text = text.replace('\t', " ")
+            text = text + '\t' + '0' + '\t' + '\n'
+            f.write(text)
+
+    f.close()
+
+    f = open('./data/IMDb_test.tsv', 'w')
+
+    path = './data/aclImdb/test/pos/'
+    for fname in glob.glob(os.path.join(path, '*.txt')):
+        with io.open(fname, 'r', encoding="utf-8") as ff:
+            text = ff.readline()
+            text = text.replace('\t', " ")
+            text = text + '\t' + '1' + '\t' + '\n'
+            f.write(text)
+
+    path = './data/aclImdb/test/neg/'
+
+    for fname in glob.glob(os.path.join(path, '*.txt')):
+        with io.open(fname, 'r', encoding="utf-8") as ff:
+            text = ff.readline()
+            text = text.replace('\t', " ")
+            text = text + '\t' + '0' + '\t' + '\n'
+            f.write(text)
+
+    f.close()
+
+    # ここから前処理
+    def preprocessing_text(text):
+
+        text = re.sub('<br />', '', text)
+
+        for p in string.punctuation:
+            if (p == ".") or (p == ","):
+                continue
+        text = text.replace(p, " ")
+
+        text = text.replace(".", " . ")
+        text = text.replace(",", " , ")
+        return text
+
+    # 分かち書き
+    def tokenizer_punctuation(text):
+
+        return text.strip().split()
+
+    def tokenizer_with_preprocessing(text):
+
+        text = preprocessing_text(text)
+        ret = tokenizer_punctuation(text)
+        return ret
+
+    print(tokenizer_with_preprocessing('I like cats.'))
+
+    # DataLoaderの作成
+    # init_token 全部の文章で文頭に入れておく単語
+    # eos_token 全部の文章で文末に入れておく単語
+
+    TEXT = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing,
+                                use_vocab=True, lower=True, include_lengths=True, batch_first=True,
+                                fix_length=max_length, init_token="<cls>", eos_token="<eos>")
+
+    LABEL = torchtext.data.Field(sequential=False, use_vocab=False)
+
+    # Datasetの作成
+    # 訓練および検証データセットを分ける。
+    train_val_ds, test_ds = torchtext.data.TabularDataset.splits(
+        path='./data/', train='IMDb_train.tsv',
+        test='IMDb_test.tsv', format='tsv',
+        fields=[('Text', TEXT), ('Label', LABEL)])
+
+    if debug_log:
+        print('訓練および検証のデータの数', len(train_val_ds))
+        print('1つ目の訓練および検証のデータ', vars(train_val_ds[0]))
+
+    # 訓練データ:検証データを8:2で分ける。
+    train_ds, val_ds = train_val_ds.split(split_ratio=0.8, random_state=random.seed(1234))
+    # これで、訓練、検証、テスト3つのDatasetの作成が完了.
+
+    # debug
+    if debug_log:
+        print('訓練データの数', len(train_ds))
+        print('検証データの数', len(val_ds))
+        print('１つ目の訓練データ', vars(train_ds[0]))
+
+    # torchtextで単語ベクトルとして英語学習済みモデルを利用する。
+    english_fasttext_vectors = Vectors(name='data/wiki-news-300d-1M.vec')
+
+    if debug_log:
+        print("1単語を表現する次元数:", english_fasttext_vectors.dim)
+        print("単語数:", len(english_fasttext_vectors.itos))
+
+    # ベクトル化したバージョンのボキャブラリーを作成する
+    TEXT.build_vocab(train_ds, vectors=english_fasttext_vectors, min_freq=10)
+
+    # ボキャブラリーのベクトルの確認を行う
+    if debug_log:
+        print(TEXT.vocab.vectors.shape)
+        print(TEXT.vocab.vectors)
+        print(TEXT.vocab.stoi)
+
+    # DataLoaderの作成
+    train_dl = torchtext.data.Iterator(train_ds, batch_size=batch_size, train=True)
+    val_dl = torchtext.data.Iterator(val_ds, batch_size=batch_size, train=False, sort=False)
+    test_dl = torchtext.data.Iterator(test_ds, batch_size=batch_size, train=False, sort=False)
+
+    # 検証データセットで確認
+    batch = next(iter(val_dl))
+
+    if debug_log:
+        print(batch.Text)
+        print(batch.Label)
+
+    return train_dl, val_dl, test_dl, TEXT
--- Transformer.py
+++ Transformer.py
@@ -1,5 +1,6 @@
-import torch.nn as nn
+import torch.nn as nn, torch, math
 from torchtext.vocab import Vectors
+import ConvertTsv
 
 
 # Embedderモジュールの実装
@@ -19,12 +20,50 @@ class Embedder(nn.module):
 
         return x_vec
 
-# 動作確認
-from utils.dataloader imoort　get_IMDb_DataLoaders_and_TEXT
 
-train_dl, val_dl, test_dl, TEXT = get_IMDb_DataLoaders_and_TEXT(
-    max_length=256, batch_size=24)
-)
+# PositionalEncodeモジュールの実装
+# 入力単語の位置情報のベクトル情報付加
+class PositionalEncoder(nn.module):
+
+    # d_model は単語ベクトルの次元数を表す
+    def __init__(self, d_model=300, max_seq_len=256):
+        super().__init__()
+
+        self.d_model = d_model
+
+        # 単語の順番posと埋め込みベクトルの次元の位置(i)によって定まる値の表をpeとして作成
+
+        pe = torch.zeros(max_seq_len, d_model)
+
+        # GPU使う用コード
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        pe = pe.to(device)
+
+        # ここも原著論文読んで後には理解したい　(Attention is all you need)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** (2 * (i + 1) / d_model)))
+
+        # peの先頭にミニバッチ次元となる次元を足す
+        self.pe = pe.unsqueeze(0)
+
+        # 勾配は計算しない
+        self.pe.requires_grad = False
+
+    def forward(self, x):
+        # 入力xとPositional Encodingの足し算
+        # 入力はpeよりも小さくなってしまうので、大きくする
+
+        ret = math.sqrt(self.d_model) * x + self.pe
+
+        return ret
+
+
+# 動作確認(Embedder)
+
+train_dl, val_dl, test_dl, TEXT = ConvertTsv.get_IMDb_DataLoaders_and_TEXT(
+    max_length=256, batch_size=24, debug_log=True)
 
 batch = next(iter(train_dl))
 
@@ -33,5 +72,19 @@ net1 = Embedder(TEXT.vocab.vectors)
 x = batch.Text[0]
 x1 = net1(x)
 
+print("動作確認(Embedder)")
+print("入力テンソルサイズ:", x.shape())
+print("出力テンソルサイズ:", x.shape())
+
+# 動作確認(Positional Encoder)
+
+net1=Embedder(TEXT.vocab.vectors)
+net2=PositionalEncoder(d_model=300,max_seq_len=256)
+
+x = batch.Text[0]
+x1=net1(x)
+x2=net2(x1)
+
+print("動作確認(Positional Encoder)")
 print("入力テンソルサイズ:", x.shape())
 print("出力テンソルサイズ:", x.shape())
